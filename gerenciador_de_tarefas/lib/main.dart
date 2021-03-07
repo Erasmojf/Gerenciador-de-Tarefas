5985ba9624951ae335951b7869f5e4d30886e869
Wings Entregas <wingsentregas@gmail.com>
2021-03-06 21:54:12 -0300

    first commit of application Task Manager
    first commit of application Task Manager

---
+++ gerenciador_de_tarefas/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:gerenciador_de_tarefas/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Gerenciador de tarefas',
+    home: HomePage(),
+  ));
+}
